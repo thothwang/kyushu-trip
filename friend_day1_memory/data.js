622e086169fb10f54137a8615b0d42a111f80dd2
thothwang <40627768+thothwang@users.noreply.github.com>
2025-11-30 11:03:45 +0800

    Add files via upload
    Day 1 html

---
+++ friend_day1_memory/data.js
@@ -0,0 +1,46 @@
+const TRIP_MEMORY_DATA = [
+  {
+    "time": "13:52",
+    "name": "一條の森ココチ",
+    "location": {
+      "lat": 33.239913922031405,
+      "lng": 130.51382741457704
+    },
+    "note": "G map上評價不多，問了Grok 發現是另一家名店（隸屬於久留米市知名連鎖「ココチ食堂」）的姊妹店，餐廳環境古宅優美，標準日式庭園被楓葉🍁包圍著，菜色擺設看起來也很日式，但剛好抵達時超過14:00已沒有午餐。\n下午茶的三明治與咖啡與甜點也是很好吃～對於午餐殘念～～～還有喝到草莓冰沙耶👍",
+    "photo": "images/1_一條の森ココチ.jpg",
+    "type": "custom"
+  },
+  {
+    "time": "18:20",
+    "name": "Matsuya Udon Honten 松屋うどん (あさくら店）",
+    "location": {
+      "lat": 33.429629567772444,
+      "lng": 130.6452613498657
+    },
+    "note": "本來想吃一頓好的～結果找的餐廳不是客滿就是沒開，超不爽，怕餓著其他人，只好速速找了一間路邊烏冬餐廳，雖然local 但烏冬是好吃的～ 我只覺得今天熱量爆表啊！",
+    "photo": "images/1_Matsuya_Udon_Honten_松屋うどん_(あさくら店）.jpg",
+    "type": "custom"
+  },
+  {
+    "time": "20:07",
+    "name": "秋月住宿Akizuki OKO art+inn",
+    "location": {
+      "lat": 33.46887342883017,
+      "lng": 130.6895596677257
+    },
+    "note": "整體風格是我喜歡的日式加侘寂風，兩個房間都有衛浴，工區還有廁所耶，有點藝廊風，廚房也讓人覺得舒適",
+    "photo": "images/1_秋月住宿Akizuki_OKO_art+inn.jpg",
+    "type": "custom"
+  },
+  {
+    "time": "20:42",
+    "name": "Family market ",
+    "location": {
+      "lat": 33.468990031387186,
+      "lng": 130.68965784637257
+    },
+    "note": "去義大利被老闆養壞的習慣，有鹹但是肉奶都香",
+    "photo": "images/1_Family_market_.jpg",
+    "type": "custom"
+  }
+];
